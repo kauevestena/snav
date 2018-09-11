3bb0dfca767bb29b7953b53ee4c0fd52c16c9344
kauevestena <kauemv2@gmail.com>
2018-09-11 14:34:09 -0300

    updates

--- drivers_script.sh
+++ drivers_script.sh
@@ -1,4 +1,15 @@
 #!/bin/dash
 
-xterm -hold -e roslaunch /home/ubuntu/snav_ws/src/ros_bno055_driver/launch/bosch_bno055_driver.launch
+#IMU DRIVER
+xterm -hold -e roslaunch /home/ubuntu/snav_ws/src/ros_bno055_driver/launch/bosch_bno055_driver.launch &
 
+sleep 5
+
+#O+W ACCES FOR CAMERAS
+echo ubuntu | sudo -S chmod o+w /dev/bus/usb/001/004
+echo ubuntu | sudo -S chmod o+w /dev/bus/usb/001/005
+
+#CAMERAS DRIVER
+xterm -hold -e roslaunch /home/ubuntu/snav_ws/snav/cameras.xml &
+
+echo "fim"
