39f9c14546f7b6dc56f8ef9d81c303d1a33e6bd4
kauevestena <kauemv2@gmail.com>
2019-01-14 20:39:10 -0200

    now GNSS is integrated

--- scripts/drivers_script.sh
+++ scripts/drivers_script.sh
@@ -11,6 +11,14 @@ sleep 5
 echo ubuntu | sudo -S chmod o+w /dev/bus/usb/001/004
 echo ubuntu | sudo -S chmod o+w /dev/bus/usb/001/005
 
+#O+W ACCES FOR GNSS RECEIVER
+echo ubuntu | sudo -S chmod o+w /dev/ttyUSB0
+
+#GNSS DRIVER
+xterm -hold -e rosrun nmea_navsat_driver nmea_serial_driver _port:=/dev/ttyUSB0 _baud:=9600 &
+
+sleep 5
+
 #CAMERAS DRIVER
 xterm -hold -e roslaunch /home/ubuntu/snav_ws/snav/launchfiles/cameras.xml &
 
--- utilitaries/gnss_rec_reader.py
+++ utilitaries/gnss_rec_reader.py
@@ -5,8 +5,8 @@ import serial, string
 output = " "
 ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
 while True:
-  print "----"
-  while output != "":
-    output = ser.readline()
-    print output
-  output = " "
+  # print "----"
+  # while output != "":
+  output = ser.readline()
+  print output
+  # output = " "
