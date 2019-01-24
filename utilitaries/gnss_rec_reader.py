455f6e8eb0556fa883d1840ff6689c42581a94df
kauevestena <kauemv2@gmail.com>
2019-01-24 15:49:05 -0200

    test on windows (u-center....)

--- utilitaries/gnss_rec_reader.py
+++ utilitaries/gnss_rec_reader.py
@@ -3,7 +3,11 @@
 import serial, string
 
 output = " "
-ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
+# ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1) #linux (default by love)
+ser = serial.Serial('\\.\COM4', 9600, 8, 'N', 1, timeout=1) #windows (tests with u-center)
+
+
+
 while True:
   # print "----"
   # while output != "":
