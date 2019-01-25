81d8116867bcc6f36caa61f56ce9885d18a40400
kauevestena <kauevestena>
2019-01-25 17:19:42 -0200

    jan 25

---
+++ offline_tools/maplab/rovio.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+source $HOME/maplab_ws/devel/setup.bash
+
+bagpath=$(head -n 1 $HOME/snav/configurations/current_bag.txt)
+
+PARAMETER_FILES_PATH="$HOME/Dropbox/Mestrado/maplab/parameter_files"
+
+LOCALIZATION_MAP_OUTPUT="$HOME/data"
+ROSBAG="$bagpath"
+NCAMERA_CALIBRATION="$PARAMETER_FILES_PATH/sensors.yaml"
+IMU_PARAMETERS_MAPLAB="$PARAMETER_FILES_PATH/imu_maplab.yaml"
+IMU_PARAMETERS_ROVIO="$PARAMETER_FILES_PATH/imu_rovio.yaml"
+REST=$@
+
+rosrun rovioli rovioli \
+  --alsologtostderr=1 \
+  --v=2 \
+  --ncamera_calibration=$NCAMERA_CALIBRATION  \
+  --imu_parameters_maplab=$IMU_PARAMETERS_MAPLAB \
+  --imu_parameters_rovio=$IMU_PARAMETERS_ROVIO \
+  --datasource_type="rosbag" \
+  --save_map_folder="$LOCALIZATION_MAP_OUTPUT" \
+  --optimize_map_to_localization_map=false \
+  --map_builder_save_image_as_resources=false \
+  --datasource_rosbag="$ROSBAG"
---
+++ offline_tools/rename_topic_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os
+
+
+with open(os.environ['HOME']+'/snav/configurations/current_bag.txt') as f:
+   	bagpath = f.readline()
+
+#original from: https://answers.ros.org/answers/13697/revisions/
+#thanks Tim Field!
+
+from rosbag import Bag
+with Bag(os.environ['HOME']+'/rosbags/renamed.bag', 'w') as Y:
+    for topic, msg, t in Bag(bagpath):
+        Y.write('imu0' if topic == 'imu/data' else topic, msg, t)
