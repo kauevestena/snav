954f21c268d0695f292b7eaec6de1c69763c8e40
kauevestena <kauemv2@gmail.com>
2019-03-08 14:55:08 -0300

    tiny edition

--- others/alternatives/calib_alternate_roscalib.bash
+++ others/alternatives/calib_alternate_roscalib.bash
@@ -7,5 +7,7 @@ roslaunch /home/ubuntu/snav_ws/snav/launchfiles/camerav2_1280x960_10fps.launch &
 
 sleep 10
 
+rosrun rqt_reconfigure rqt_reconfigure
+
 rosrun camera_calibration cameracalibrator.py --size 9x6 --square 0.03 image:=/raspicam_node/image camera:=raspicam_node --fix-aspect-ratio -k 2 --zero-tangent-dist
 
