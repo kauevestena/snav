21528acd63acb558b272141a26bc2c7dd33dd842
kauevestena <kauemv2@gmail.com>
2019-06-05 19:11:20 -0300

    more stuff

--- scripts/drivers_script.sh
+++ scripts/drivers_script.sh
@@ -14,7 +14,7 @@ sleep 5
 
 #O+W ACCES FOR CAMERAS
 # echo ubuntu | sudo -S chmod o+w /dev/bus/usb/001/004
-# echo ubuntu | sudo -S chmod o+w /dev/bus/usb/001/005
+echo ubuntu | sudo -S chmod o+w /dev/bus/usb/001/005
 
 #O+W ACCES FOR GNSS RECEIVER
 echo ubuntu | sudo -S chmod o+w /dev/ttyUSB0
@@ -25,6 +25,7 @@ xterm -hold -e rosrun nmea_navsat_driver nmea_serial_driver _port:=/dev/ttyUSB0 
 sleep 5
 
 #CAMERAS DRIVER
+xterm -hold -e roslaunch $HOME/snav_ws/snav/launchfiles/logi_cam.xml &
 xterm -hold -e roslaunch $HOME/snav_ws/snav/launchfiles/camerav2_1280x960.launch &
 
 echo "fim"
