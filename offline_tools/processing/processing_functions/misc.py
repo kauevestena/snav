04082f441abc0064975aa5539ddceca0ade88540
kauevestena <kauemv@gmail.com>
2019-07-23 01:47:45 -0300

    telegram bot support included

--- offline_tools/processing/processing_functions/misc.py
+++ offline_tools/processing/processing_functions/misc.py
@@ -3,6 +3,7 @@ import os
 import ntpath
 import glob
 import numpy as np
+import requests
 
 
 def find_datetime_String(inputStr,onlyDate=False):
@@ -76,3 +77,17 @@ def listOfLists_npArrays(inputList):
     for item in inputList:
         outList.append(np.array(item))
     return outList
+
+def telegram_bot_sendtext(bot_message):
+    
+    # thx: https://is.gd/gaFK8s (Man Hay Hong)
+    configFilePath  = os.path.join(os.environ['HOME'],"Dropbox/telegram_bot.txt")
+
+    storageFile = open(configFilePath,'r')
+    data = storageFile.readline()
+
+    bot_token = data.split(',')[0]
+    bot_chatID = data.split(',')[1]
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
--- offline_tools/processing/tests.py
+++ offline_tools/processing/tests.py
@@ -3,16 +3,26 @@ import cv2
 from processing_functions import general_funcs as gf
 from processing_functions import color_stuff as cs
 from processing_functions import misc as msc
+import requests
+import os
 
-# test = [[293,3,0],[234,22]]
+import requests
 
-# print(msc.listOfLists_npArrays(test))
+def telegram_bot_sendtext(bot_message):
+    
+    # thx: https://is.gd/gaFK8s (Man Hay Hong)
+    configFilePath  = os.path.join(os.environ['HOME'],"Dropbox/telegram_bot.txt")
 
+    storageFile = open(configFilePath,'r')
+    data = storageFile.readline()
 
-imPath = "/home/kauevestena/tests/CamVid/test_labels/0006R0_f01380_L.png"
+    bot_token = data.split(',')[0]
+    bot_chatID = data.split(',')[1]
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
-labels = [[0,128,128],[0,192,192]]
-labels = msc.listOfLists_npArrays(labels)
+    response = requests.get(send_text)
 
-cs.remove_other_classes2(imPath,"test.png",labels)
+    
+
+test = telegram_bot_sendtext("oieeeeeee")
 
