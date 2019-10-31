25f9a9b62fa4050891a5488d3e7e24f6f48369c3
kauevestena <kauevestena>
2019-10-31 17:05:03 -0300

    validation class

--- offline_tools/processing/evaluation.py
+++ offline_tools/processing/evaluation.py
@@ -1,15 +1,32 @@
 import os
+from processing_functions import validation as vd
 from processing_functions import misc as msc
 
 
-OPJ = os.path.join
-OE = os.environ
+# OPJ = os.path.join
+# OE = os.environ
 
 # paths that contain subfolders with checkpoints
 pathsWithCheckpoints = [
-    msc.joinToHome("/data/checkpoints/deeplab_plus"),
+    msc.joinToHome("/Dropbox/data/checkpoints/deeplab_plus"),
     "/media/kauevestena/data/Semantic-Segmentation-Suite/checkpoints",
     ]
+listsOfCkptFolders = []
+for ckptPath in pathsWithCheckpoints:
+    # print(ckptPath)
+    listsOfCkptFolders.append(msc.getSubdirs(ckptPath))
+# print(listsOfCkptFolders)
+# removing invalid folders:
+validCkptFolders = []
+
+for listOfCkpt in listsOfCkptFolders:
+    for ckptFolder in listOfCkpt:
+        if(vd.check_ckpt_folder(ckptFolder)): #the filtering function
+            validCkptFolders.append(ckptFolder)
+        # else:
+        #     print(ckptFolder)
+
+print(validCkptFolders)
 
 # Ground Truth Images
 gtImages = msc.joinToHome("/Dropbox/data/gt/originals")
--- offline_tools/processing/processing_functions/validation.py
+++ offline_tools/processing/processing_functions/validation.py
@@ -2,9 +2,29 @@ import cv2
 import os
 import sys
 
+
+
+def check_ckpt_folder(ckptfolderpath):
+    checkpoint_info  = os.path.join(ckptfolderpath,'checkpoint') 
+    checkpoint_model = os.path.join(ckptfolderpath,'model.ckpt.index') 
+
+    if os.path.isfile(checkpoint_info) and os.path.isfile(checkpoint_model):
+        return True
+    else:
+        return False
+
 class checkpoint:
     supported_models = {"FC-DenseNet56":"FC-DenseNet56", "FC-DenseNet67":"FC-DenseNet67", "FC-DenseNet103":"FC-DenseNet103", "Encoder-Decoder":"Encoder-Decoder", "Encoder-Decoder-Skip":"Encoder-Decoder-Skip","RefineNet":"RefineNet", "FRRN-A":"FRRN-A", "FRRN-B":"FRRN-B", "MobileUNet":"MobileUNet", "MobileUNet-Skip":"MobileUNet-Skip", "PSPNet":"PSPNet", "GCN":"GCN", "DeepLabV3":"DeepLabV3", "DeepLabV3_plus":"DeepLabV3_plus", "AdapNet":"AdapNet","DenseASPP":"DenseASPP", "DDSC":"DDSC", "BiSeNet":"BiSeNet", "custom":"custom"}
 
-    def __init__(self, path, model_entry):
-        self.path = path
-        self.model_entry = self.supported_models[model_entry]
+    infos_suffix = 'checkpoint'
+    ckpt_suffix  = 'model.ckpt'
+
+    def __init__(self, basepath):
+        self.basepath = basepath
+        self.infospath   = os.path.join(self.basepath,self.infos_suffix)
+        self.ckpt_suffix = os.path.join(self.basepath,self.ckpt_suffix) 
+
+
+
+
+        # self.model_entry = self.supported_models[model_entry]
